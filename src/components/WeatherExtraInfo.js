5262608dfebeaca8a1923afbea5fd23fc7fc43ea
ivan <c.ivan.felix.r@gmail.com>
2019-01-16 00:45:06 -0600

    some components

---
+++ src/components/WeatherExtraInfo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const WeatherExtraInfo = () => (
+   <div>Informacion extra</div>
+)
+
+export default WeatherExtraInfo;
---
+++ src/components/WeatherTemperature.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const WeatherTemperature = () => (
+   <div><span>25 grades</span></div>
+);
+
+
+export default WeatherTemperature;
