3daa7a6e800136a3711dc7f20852b1491badec8e
ivan <c.ivan.felix.r@gmail.com>
2019-01-17 21:21:19 -0600

    refactory directories

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,7 +4,7 @@ import WeatherData from './WeatherData';
 
 const WeatherLocation = () => (
    <div>
-      <Location city={"Testo2"}></Location>
+      <Location city={"Mexico"}></Location>
       <WeatherData></WeatherData>
    </div>
 );
