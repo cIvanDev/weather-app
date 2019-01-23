8e328cf7c251f1809d5fd13c71e0d92b1b215015
ivan <c.ivan.felix.r@gmail.com>
2019-01-22 19:22:26 -0600

    setState / class component

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-import {
-   CLOUD,
-   //CLOUDY,
-   //SUN, 
-   //RAIN,
-   //SNOW,
-   //WINDY,
-} from './../../../constants/weathers';
 import './styles.css';
 
-
-
-const WeatherData = () => (
+const WeatherData = ({ data: {  temperature, weatherState, humidity, wind  } }) => (
    <div className="weatherDataCont">
-         <WeatherTemperature 
-            temperature={20} 
-            weatherState={CLOUD}/>
-      <WeatherExtraInfo humidity={80} wind={"10 m/s"}/>
+      <WeatherTemperature 
+         temperature={temperature} 
+         weatherState={weatherState}/>
+      <WeatherExtraInfo humidity={humidity} wind={wind}/>
    </div>
 );
+
+WeatherData.propTypes = {
+   data: PropTypes.shape({
+      temperature: PropTypes.number.isRequired,
+      weatherState: PropTypes.string.isRequired,
+      humidity: PropTypes.number.isRequired,
+      wind: PropTypes.string.isRequired,
+   })
+}
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
+import {
+   CLOUD,
+   //CLOUDY,
+   //SUN, 
+   //RAIN,
+   //SNOW,
+   //WINDY,
+} from './../../constants/weathers';
 
-const WeatherLocation = () => (
-   <div className="weatherLocationCont">
-      <Location city={"Mexico"}></Location>
-      <WeatherData></WeatherData>
-   </div>
-);
+const data = {
+   temperature: 8,
+   weatherState: CLOUD,
+   humidity: 10,
+   wind: '10 m/s',
+}
+
+class WeatherLocation extends Component {
+   constructor(){
+      super();
+      this.state = {
+         city: "Espania",
+         data: data,
+      };
+   }
+
+
+   handleUpdateClick = () => {
+      console.log("actualizadu");
+
+
+      this.setState({ city: 'Outra cidade'})
+   }
+
+   render(){
+      const { city, data } = this.state;
+
+      return(
+         <div className="weatherLocationCont">
+            <Location city={city}></Location>
+            <WeatherData data={data}></WeatherData>
+            <button onClick={this.handleUpdateClick}>Actualizar</button>
+         </div>
+      )
+   }
+
+};
 
 export default WeatherLocation;
